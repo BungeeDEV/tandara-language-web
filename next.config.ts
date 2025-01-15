5497373c448d7ba7dc9f9a50fbb32f7ff5174b2f
BungeeDEV <kilian.drisga@outlook.de>
2025-01-15 16:55:40 +0100

    Changed next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    output: "export",
     env: {
         NEXT_PUBLIC_ADMIN_USERNAME: process.env.ADMIN_USERNAME,
         NEXT_PUBLIC_ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
